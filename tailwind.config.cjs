239cad720b9fb6192e8d6ebff1c3a4a34e20527d
nemog9 <tomone.ameno@gmail.com>
2023-07-08 23:49:13 +0900

    change font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -57,7 +57,8 @@ module.exports = {
       transparent: "transparent",
     },
     fontFamily: {
-      mono: ["IBM Plex Mono", "monospace"],
+      // mono: ["monospace"],
+      jp: ["Noto Sans JP"]
     },
     // },
   },
